e024c529ef987b7537305c068a3fbe920f923e31
martinmt-mx <mmojica0@ucol.mx>
2020-10-06 21:01:39 -0500

    correcciones

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 var arr = []
-for (i = 0; i<=999; i++){
+for (let i = 0; i<=999; i++){
     arr[i]=true
 }
 
-for (i = 2; i <= 999; i++){
+for (let i = 2; i <= 999; i++){
     if(arr[i]==true){
-        for(ii = i; i * ii <= 999; ii++){
+        for(let ii = i; i * ii <= 999; ii++){
             arr[i*ii] = false
         }
+    }
+}
+
+for (let i = 2; i <= 999; i++){
+    if(arr[i]==true){
         console.log(i)
     }
 }
